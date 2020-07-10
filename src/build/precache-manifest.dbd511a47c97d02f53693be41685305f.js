19acc938160370f0b1490a1554c111cbca8eb343
hgantner <hgantner@hgantner.local>
2020-07-10 17:15:48 -0500

    First commit

---
+++ src/build/precache-manifest.dbd511a47c97d02f53693be41685305f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5456fa6a78172010dec173c11224725",
+    "url": "/index.html"
+  },
+  {
+    "revision": "840897ce5b2f011f3587",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "e4d5353e9f1a186746ef",
+    "url": "/static/js/2.3286aa40.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3286aa40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "840897ce5b2f011f3587",
+    "url": "/static/js/main.8fad7d1a.chunk.js"
+  },
+  {
+    "revision": "dd8147bbe7734801bdde",
+    "url": "/static/js/runtime-main.e312f208.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
